4e7b1a0e5ec680f7a3cc706a368e09f7b6158466
michael <mikemboya2@gmail.com>
2020-02-06 03:58:23 +0000

    URLs Class

---
+++ app/src/main/java/Volley/URLs.java
@@ -0,0 +1,7 @@
+package Volley;
+
+public class URLs {
+    public static final String ROOT_URL = "https://mellborngroup.co.ke/api";
+    public static final String SIGN_UP = ROOT_URL + "/signup.php";
+    public static final String SIGN_IN = ROOT_URL + "/signin.php";
+}
