5b97541050892edfff1499831c50c56d1bd6d71a
michael <mikemboya2@gmail.com>
2020-02-13 10:39:55 +0000

    Layout Changes

--- app/src/main/java/com/cemesinsurance/cemes_customer/Fragment_Motor_Insurance.java
+++ app/src/main/java/com/cemesinsurance/cemes_customer/Fragment_Motor_Insurance.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
@@ -13,6 +15,10 @@ import android.view.ViewGroup;
  */
 public class Fragment_Motor_Insurance extends Fragment {
 
+    Spinner carUseSpinner;
+    Spinner carClassSpinner;
+    Spinner carModelSpinner;
+    Spinner carMakeSpinner;
 
     public Fragment_Motor_Insurance() {
         // Required empty public constructor
@@ -23,7 +29,19 @@ public class Fragment_Motor_Insurance extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_motor_insurance, container, false);
+        View view = inflater.inflate(R.layout.fragment_motor_insurance, container, false);
+
+        // Set up views
+        carUseSpinner = view.findViewById(R.id.carUseSpinner);
+        carClassSpinner = view.findViewById(R.id.carClassSpinner);
+        carModelSpinner = view.findViewById(R.id.carModelSpinner);
+        carMakeSpinner = view.findViewById(R.id.carMakeSpinner);
+
+        // Adapters for the spinners
+        ArrayAdapter<CharSequence>  carClassAdapter = ArrayAdapter.createFromResource(view.getContext(), R.array.car_classes, android.R.layout.simple_spinner_item);
+        carClassAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        carClassSpinner.setAdapter(carClassAdapter);
+        return view;
     }
 
 }
