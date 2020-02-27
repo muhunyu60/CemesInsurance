1d199f5c582b11c9d8ca70e8d65e2d8d3d274cb8
michael <mikemboya2@gmail.com>
2020-02-27 06:25:56 +0000

    Travel Insurance Layout

---
+++ app/src/main/java/fragment/Fragment_Travel_Insurance.java
@@ -0,0 +1,25 @@
+package fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cemesinsurance.cemes_customer.R;
+
+
+public class Fragment_Travel_Insurance extends Fragment {
+
+    public Fragment_Travel_Insurance() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_travel_insurance, container, false);
+    }
+}
