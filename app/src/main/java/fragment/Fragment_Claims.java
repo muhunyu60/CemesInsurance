782212808915817e81e2485ab286ed818866545a
michael <mikemboya2@gmail.com>
2020-03-03 10:08:06 +0000

    Profile UI changes

--- app/src/main/java/fragment/Fragment_Claims.java
+++ app/src/main/java/fragment/Fragment_Claims.java
@@ -2,6 +2,10 @@ package fragment;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -82,7 +86,24 @@ public class Fragment_Claims extends Fragment {
             }
         });
 
+//        floatingActionButton.setImageBitmap(textAsBitmap("Make Claim", 200, Color.WHITE));
+
         return view;
+    }
+
+    public static Bitmap textAsBitmap(String text, float textSize, int textColor) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setTextSize(textSize);
+        paint.setColor(textColor);
+        paint.setTextAlign(Paint.Align.LEFT);
+        float baseline = -paint.ascent();
+        int width = (int)  (paint.measureText(text) + 0.0f);
+        int height = (int) (baseline + paint.descent() + 0.0f);
+        Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(image);
+        canvas.drawText(text, 0, baseline, paint);
+        return image;
     }
 
 }
