d3982fd8f6e299a4d68b6de3d851962f6450f90e
michael <mikemboya2@gmail.com>
2020-03-03 11:14:50 +0000

    Populating the claim type spinner

--- app/src/main/java/com/cemesinsurance/cemes_customer/Add_Claim.java
+++ app/src/main/java/com/cemesinsurance/cemes_customer/Add_Claim.java
@@ -2,12 +2,21 @@ package com.cemesinsurance.cemes_customer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Add_Claim extends AppCompatActivity {
+    Spinner insuranceTypeSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__claim);
+
+        ArrayAdapter<CharSequence>  claimTypeAdapter = ArrayAdapter.createFromResource(this, R.array.claim_cover_type, android.R.layout.simple_spinner_item);
+        claimTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        insuranceTypeSpinner = findViewById(R.id.claimTypeSpinner);
+        insuranceTypeSpinner.setAdapter(claimTypeAdapter);
     }
 }
