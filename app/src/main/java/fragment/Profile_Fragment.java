c372a06410d3747f538a3d6fc58b511c9b73d267
michael <mikemboya2@gmail.com>
2020-03-03 12:22:10 +0000

    Set edit texts to logged in user details

--- app/src/main/java/fragment/Profile_Fragment.java
+++ app/src/main/java/fragment/Profile_Fragment.java
@@ -1,17 +1,27 @@
 package fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.cemesinsurance.cemes_customer.R;
+
+import model.User;
+import state.SharedPrefManager;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Profile_Fragment extends Fragment {
+
+    private EditText nameEditText;
+    private EditText phoneEditText;
+    private EditText emailEditText;
+    private User user;
 
     public Profile_Fragment() {
         // Required empty public constructor
@@ -22,6 +32,20 @@ public class Profile_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Initialise variables
+        nameEditText =  view.findViewById(R.id.profileNameEditText);
+        phoneEditText = view.findViewById(R.id.profilePhoneEditText);
+        emailEditText = view.findViewById(R.id.profileEmailEditText);
+        user = SharedPrefManager.getInstance(getActivity()).getUser();
+
+        // Set the editText values based on the current logged in user credentials
+        nameEditText.setText(user.getName());
+        phoneEditText.setText(user.getPhone());
+        emailEditText.setText(user.getEmail());
+
+        return view;
     }
+
 }
