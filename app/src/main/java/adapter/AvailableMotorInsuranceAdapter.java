6e984f81041c8bb003c0e1cacad4787091c5be7a
michael <mikemboya2@gmail.com>
2020-03-12 12:37:42 +0000

    Adapter for the available motor insurance policies that are to populate the recycler

---
+++ app/src/main/java/adapter/AvailableMotorInsuranceAdapter.java
@@ -0,0 +1,32 @@
+package adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AvailableMotorInsuranceAdapter extends RecyclerView.Adapter {
+
+    private class AvailableMotorInsuranceViewHolder extends RecyclerView.ViewHolder{
+
+        public AvailableMotorInsuranceViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
