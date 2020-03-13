a972c8e2d9abaaf314a6ec6fe4f0baeb808efa27
michael <mikemboya2@gmail.com>
2020-03-13 09:15:14 +0000

    Adapter for the available motor insurance policies recycler

--- app/src/main/java/adapter/AvailableMotorInsuranceAdapter.java
+++ app/src/main/java/adapter/AvailableMotorInsuranceAdapter.java
@@ -2,26 +2,47 @@ package adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.cemesinsurance.cemes_customer.R;
 
 public class AvailableMotorInsuranceAdapter extends RecyclerView.Adapter {
 
     public class AvailableMotorInsuranceViewHolder extends RecyclerView.ViewHolder{
+        TextView price;
+        TextView insuranceTitle;
+        TextView windScreen;
+        TextView radio;
+        TextView excessProtector;
+        TextView PVT;
+        TextView lossOfValue;
 
         public AvailableMotorInsuranceViewHolder(View itemView) {
             super(itemView);
+            price = itemView.findViewById(R.id.textViewPrice);
+            insuranceTitle = itemView.findViewById(R.id.textViewInsuranceCompany);
+            windScreen = itemView.findViewById(R.id.textViewWindScreenValue);
+            radio = itemView.findViewById(R.id.textViewRadioValue);
+            excessProtector = itemView.findViewById(R.id.textViewExcessProtectorValue);
+            PVT = itemView.findViewById(R.id.textViewPVTValue);
+            lossOfValue = itemView.findViewById(R.id.textViewLossOfUseValue);
         }
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.available_motor_insurance_item, parent, false);
+        AvailableMotorInsuranceViewHolder viewHolder = new AvailableMotorInsuranceViewHolder(v);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
 
     }
 
