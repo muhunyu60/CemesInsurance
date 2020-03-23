9a14a85439fe7edf8c7a0d2739de4972ed7c7f61
michael <mikemboya2@gmail.com>
2020-03-23 09:16:05 +0000

    Passing data between dialogfragment and motorresultActivity

--- app/src/main/java/com/cemesinsurance/cemes_customer/MotorResultActivity.java
+++ app/src/main/java/com/cemesinsurance/cemes_customer/MotorResultActivity.java
@@ -8,10 +8,25 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import adapter.AvailableMotorInsuranceAdapter;
+import customfonts.MyTextView_SF_Pro_Display_Medium;
 import fragment.MotorExtraOptionsDialogFragment;
 
-public class MotorResultActivity extends AppCompatActivity {
+public class MotorResultActivity extends AppCompatActivity implements MotorExtraOptionsDialogFragment.MotorExtraOptionsDialogListener {
     CardView extraOptionsCardView;
+
+    MyTextView_SF_Pro_Display_Medium windScreenValueTextView;
+    MyTextView_SF_Pro_Display_Medium PVTValueTextView;
+    MyTextView_SF_Pro_Display_Medium radioValueTextView;
+    MyTextView_SF_Pro_Display_Medium excessProtectorValueTextView;
+    MyTextView_SF_Pro_Display_Medium roadRescueValueTextView;
+    MyTextView_SF_Pro_Display_Medium lossOfUseValueTextView;
+
+    String windScreenValue = "0";
+    String PVTValue = "0";
+    String radioValue = "0";
+    String excessProtectorValue = "0";
+    String roadRescueValue = "0";
+    String lossOfUseValue = "0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +44,39 @@ public class MotorResultActivity extends AppCompatActivity {
                 showExtraOptionsDialogFragment();
             }
         });
+
+        windScreenValueTextView = findViewById(R.id.windscreenExampleValueTextView);
+        PVTValueTextView = findViewById(R.id.pvtExampleValueTextView);
+        radioValueTextView = findViewById(R.id.radioExampleValueTextView);
+        excessProtectorValueTextView = findViewById(R.id.excessProtectorExampleValueTextView);
+        roadRescueValueTextView = findViewById(R.id.roadRescueExampleValueTextView);
+        lossOfUseValueTextView = findViewById(R.id.lossOfUseExampleValueTextView);
+
+        setTextViewValues();
     }
 
     public void showExtraOptionsDialogFragment() {
         MotorExtraOptionsDialogFragment dialogFragment = new MotorExtraOptionsDialogFragment();
         dialogFragment.show(getSupportFragmentManager(), "Extra Motor Options");
+    }
+
+    public void setTextViewValues() {
+        windScreenValueTextView.setText(windScreenValue);
+        PVTValueTextView.setText(PVTValue);
+        radioValueTextView.setText(radioValue);
+        excessProtectorValueTextView.setText(excessProtectorValue);
+        roadRescueValueTextView.setText(roadRescueValue);
+        lossOfUseValueTextView.setText(lossOfUseValue);
+    }
+
+    @Override
+    public void passOptions(String windscreenValue, String radioValue, Boolean excessProtector, Boolean politicalViolenceAndTerrorism, Boolean lossOfUse, Boolean roadRescue) {
+        this.windScreenValue = windscreenValue;
+        this.radioValue = radioValue;
+        excessProtectorValue = excessProtector ? "yes" : "no";
+        PVTValue = politicalViolenceAndTerrorism ? "yes" : "no";
+        lossOfUseValue = lossOfUse ? "yes" : "no";
+        roadRescueValue = lossOfUse ? "yes" : "no";
+        setTextViewValues();
     }
 }
--- app/src/main/java/fragment/MotorExtraOptionsDialogFragment.java
+++ app/src/main/java/fragment/MotorExtraOptionsDialogFragment.java
@@ -2,6 +2,7 @@ package fragment;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialogFragment;
@@ -19,7 +20,7 @@ public class MotorExtraOptionsDialogFragment extends AppCompatDialogFragment {
     CheckBox politicalViolenceAndTerrorismCheckBox;
     CheckBox lossOfUseCheckBox;
     CheckBox roadRescueCheckBox;
-
+    MotorExtraOptionsDialogListener listener;
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         LayoutInflater inflater = getActivity().getLayoutInflater();
@@ -37,7 +38,27 @@ public class MotorExtraOptionsDialogFragment extends AppCompatDialogFragment {
                 .setPositiveButton("ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
+                        String windscreenValue = windScreenEditText.getText().toString();
+                        if (windscreenValue.trim().length() == 0)  {
+                            windscreenValue = "0";
+                        }
+                        String radioValue = radioEditText.getText().toString();
+                        if (radioValue.trim().length() == 0)  {
+                            radioValue = "0";
+                        }
+                        Boolean excessProtector = excessProtectorCheckBox.isChecked();
+                        Boolean politicalViolenceAndTerrorism = politicalViolenceAndTerrorismCheckBox.isChecked();
+                        Boolean lossOfUse = lossOfUseCheckBox.isChecked();
+                        Boolean roadRescue = roadRescueCheckBox.isChecked();
 
+                        listener.passOptions(
+                                windscreenValue,
+                                radioValue,
+                                excessProtector,
+                                politicalViolenceAndTerrorism,
+                                lossOfUse,
+                                roadRescue
+                        );
                     }
                 })
                 .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
@@ -48,5 +69,24 @@ public class MotorExtraOptionsDialogFragment extends AppCompatDialogFragment {
                 });
 
         return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            listener = (MotorExtraOptionsDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + "Must implement passOptions");
+        }
+    }
+
+    public interface MotorExtraOptionsDialogListener {
+        void passOptions(String windscreenValue,
+                         String radioValue,
+                         Boolean excessProtector,
+                         Boolean politicalViolenceAndTerrorism,
+                         Boolean lossOfUse,
+                         Boolean roadRescue);
     }
 }
