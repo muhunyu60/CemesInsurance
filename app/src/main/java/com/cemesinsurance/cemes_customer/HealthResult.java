9046d777625e99fe1ee8e630a2f51440b5a53da6
michael <mikemboya2@gmail.com>
2020-04-24 06:28:20 +0000

    Setting up recycler view

--- app/src/main/java/com/cemesinsurance/cemes_customer/HealthResult.java
+++ app/src/main/java/com/cemesinsurance/cemes_customer/HealthResult.java
@@ -2,6 +2,7 @@ package com.cemesinsurance.cemes_customer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class HealthResult extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class HealthResult extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_result);
+
+        RecyclerView recyclerView = findViewById(R.id.health_recycler_view);
     }
 }
