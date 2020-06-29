ba8e93d1e85bbf75a9606fb934be71018cee073e
michael <mikemboya2@gmail.com>
2020-06-29 07:20:30 +0000

    Layout files for policies

---
+++ app/src/main/java/com/cemesinsurance/cemes_customer/Fragment_Policies.java
@@ -0,0 +1,21 @@
+package com.cemesinsurance.cemes_customer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_Policies extends Fragment {
+
+    public Fragment_Policies() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment__policies, container, false);
+    }
+}
